27d884b3c6a9be94adafe03b8583293dd0cd6c91
Lena Volnuhina <lenaw04@mail.ru>
2020-04-17 14:49:31 +0300

    Предварительно: работает, орбиту спутника видно.
    Без учета небесных тел. Почему то луна и солнце вносят дичайшие ошибки (можно потестить в файле F все еще есть коды)

--- simulation/RungKUTT.m
+++ simulation/RungKUTT.m
@@ -1,17 +1,20 @@
-function [RungKUTT ] = RungKUTT( t,dt,xa,ya,za,vxa,vya,vza, T, )
-for i=1:lenght(t)
-    inp=[ xa,ya,za,vxa,vya,vza];
+function [RungKUTT ] = RungKUTT( t,dt,xa,ya,za,vxa,vya,vza, T )
+ inp=[ xa,ya,za,vxa,vya,vza];
+for i=1:length(t)
+    
+  %  inp=[ xa,ya,za,vxa,vya,vza];
     
  
-    K0=F(inp,const);
+    K0=F(inp(i,:),T);
+  
     
-    K1=F(inp+0.5*dt.*K1, T);
+    K1=F((inp(i,:)+0.5*dt.*K0), T);
     
-    K2=F(inp+0.5*dt.*K2, T);
+    K2=F(inp(i,:)+0.5*dt.*K1, T);
     
-    K3=F(inp+dt.*K3, T);
+    K3=F(inp(i,:)+dt.*K2, T);
     
-    inp(i+1)=inp(i)+(dt/6)*(K0+2*K1+2*K2+K3);
+    inp(i+1,:)=inp(i,:)+(dt/6)*(K0+2*K1+2*K2+K3);
     
 end
 [RungKUTT]=inp;
--- simulation/math.m
+++ simulation/math.m
@@ -1,5 +1,42 @@
-function [math ] = (xa,ya,za,vxa,vya,vza,Jsm_x,Jsm_y,Jsm_z,time_start,time_final,  )
+function [math ] = math(xa,ya,za,vxa,vya,vza,Jsm_x,Jsm_y,Jsm_z,time_start,time_final, te,T)
+%теперь надо раздеаться с временами, т.к. часть вычислений надо сделать на
+%более раннее время, а часть на более позднее
+%для раннего времени
+dt=1;
+if(te<=time_start&&te<time_final)
+    t=time_start:dt:time_final-dt;
+    %result=nan(length(t),6);
+    XA=zeros(length(t),1);
+    XA(1,:)=xa;
+    YA=zeros(length(t),1);
+    YA(1,:)=ya;
+    ZA=zeros(length(t),1);
+    ZA(1,:)=za;
+    VXA=zeros(length(t),1);
+    VXA(1,:)=vxa;
+    VYA=zeros(length(t),1);
+    VYA(1,:)=vya;
+    VZA=zeros(length(t),1);
+    VZA(1,:)=vza;
+    result=RungKUTT( t,dt,XA,YA,ZA,VXA,VYA,VZA, T ); 
+elseif(te>time_start&&te<time_final)
+    t1=te:dt:time_final-dt;
+    %result=nan(length(t1),6);
+    XA=zeros(length(t1),1);
+    XA(1,:)=xa;
+    YA=zeros(length(t1),1);
+    YA(1,:)=ya;
+    ZA=zeros(length(t1),1);
+    ZA(1,:)=za;
+    VXA=zeros(length(t1),1);
+    VXA(1,:)=vxa;
+    VYA=zeros(length(t1),1);
+    VYA(1,:)=vya;
+    VZA=zeros(length(t1),1);
+    VZA(1,:)=vza;
+    result=RungKUTT( t1,dt,XA,YA,ZA,VXA,VYA,VZA, T ); 
+end
 
-
+[math]=result;
 end
 
